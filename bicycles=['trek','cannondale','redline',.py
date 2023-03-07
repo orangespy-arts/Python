c00cce4c6292945f75db5f6f46ec4a16ae9b5494
Zhanlan Wang <98122341+2passionfruit@users.noreply.github.com>
2023-03-07 14:12:00 +0000

    update

---
+++ bicycles=['trek','cannondale','redline',.py
@@ -0,0 +1,8 @@
+bicycles=['trek','cannondale','redline','specialzied']
+print(bicycles)
+# accessing elements in a list
+print(bicycles[0])
+print(bicycles[1].title())
+# print the last one 
+print(bicycles[-1])
+print(bicycles[-2])
